56763e4111af25bcd1073934ea1d696178828e33
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2021-02-18 18:02:37 +0100

    Update desmark-dev.js

--- src/js/desmark-dev.js
+++ src/js/desmark-dev.js
@@ -1,91 +1,208 @@
 
-var game = new Phaser.Game(448, 496, Phaser.AUTO);
-// Création de la class  du personnage
-var desmarkDev = function (game) 
-{
+export default class DesmarkDev {
+    constructor(scene, position, anim, dieCallback)
+    {       
+        this.sprite=scene.physics.add.sprite(position.x, position.y, 'pacman')
+            .setScale(0.9)
+            .setOrigin(0.5);
+        this.spawnPoint=position;
+        this.anim=anim;        
+        this.dieCallback=dieCallback;
+        this.speed = 95;
+        this.moveTo = new Phaser.Geom.Point();
+        this.sprite.angle = 180;
+        this.safetile = [-1, 18];
+        this.directions = [];
+        this.opposites = [ null, null, null, null, null, Phaser.DOWN, Phaser.UP, Phaser.RIGHT, Phaser.LEFT ];        
+        this.turning=Phaser.NONE;
+        this.current = Phaser.NONE;
+        this.turningPoint = new Phaser.Geom.Point();
+        this.threshold = 5;
+        this.life = 3;
+        this.score=0;
+        this.active=true;
+        this.sprite.anims.play(this.anim.Stay, true);
+        let ref=this;
+        this.sprite.on('animationcomplete', function(animation, frame) {
+            ref.animComplete(animation, frame);
+        }, scene);
+        this.playing = false;
+    }
 
-    this.map = null;
-    this.layer = null;
-    this.pacman = null;
+    die() {
+        this.active=false;
+        this.playing=false;
+        this.life--;
+        this.moveTo = new Phaser.Geom.Point();
+        this.sprite.anims.play(this.anim.Die, true);
+    }
 
-    this.safetile = 14;
-    this.gridsize = 16;
+    animComplete (animation, frame)
+    {        
+        if(animation.key==this.anim.Die) {
+            this.dieCallback();
+        }
+    }
 
-    this.speed = 150;
-    this.threshold = 3;
+    respawn() {
+        this.active=true;
+        this.playing = false;
+        this.sprite.setPosition(this.spawnPoint.x, this.spawnPoint.y);
+        this.moveTo = new Phaser.Geom.Point();
+        this.sprite.anims.play(this.anim.Stay, true);
+        this.sprite.angle = 180;
+        this.turning = Phaser.NONE;
+        this.current = Phaser.NONE;
+    }
 
-    this.marker = new Phaser.Point();
-    this.turnPoint = new Phaser.Point();
-
-    this.directions = [ null, null, null, null, null ];
-    this.opposites = [ Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP ];
-
-    this.current = Phaser.NONE;
-    this.turning = Phaser.NONE;
-
-};
-desmarkDev.prototype = 
-{
-
-    init: function () 
+    moveLeft()
     {
+        this.moveTo.x=-1;
+        this.moveTo.y=0;
+        this.sprite.anims.play(this.anim.Eat, true);
+        this.sprite.angle = 180;
+    }
 
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
-
-        Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
-
-        this.physics.startSystem(Phaser.Physics.ARCADE);
-
-    },
-
-
-    preload: function () 
+    moveRight()
     {
-        this.load.image("mur", "asset/mur0.png","asset/mur1.png","asset/mur2.png","asset/mur3.png",
-        "asset/mur4.png","asset/mur5.png", "assets/mur6.png","asset/mur7.png","asset/mur8.png","asset/mur9.png",
-        "asset/mur10.png","asset/mur11.png","asset/mur12.png","asset/mur13.png","asset/mur14.png","asset/mur15.png");
-        this.load.spritesheet('Desmark-Dev', 'asset/curseur.png' );
+        this.moveTo.x=1;
+        this.moveTo.y=0;
+        this.sprite.anims.play(this.anim.Eat, true);
+        this.sprite.angle = 0;
+    }
+
+    moveUp()
+    {
+        this.moveTo.x=0;
+        this.moveTo.y=-1;
+        this.sprite.anims.play(this.anim.Eat, true);
+        this.sprite.angle = 270;
+    }
+
+    moveDown()
+    {
+        this.moveTo.x=0;
+        this.moveTo.y=1;
+        this.sprite.anims.play(this.anim.Eat, true);
+        this.sprite.angle = 90;
+    }
+
+    update()
+    {
+        this.sprite.setVelocity(this.moveTo.x * this.speed,  this.moveTo.y * this.speed);
+        this.turn();
+        if(this.directions[this.current] && !this.isSafe(this.directions[this.current].index)) {
+            this.sprite.anims.play('faceRight', true);
+        }
+    }
+
+    setDirections(directions) {
+        this.directions = directions;
+    }
+
+    setTurningPoint(turningPoint) {
+        this.turningPoint=turningPoint;
+    }
+
+
+    setTurn(turnTo)
+    {
+        if (!this.active || !this.directions[turnTo] || this.turning === turnTo || this.current === turnTo || !this.isSafe(this.directions[turnTo].index)) {
+            return false;
+        }
+
+        if(this.opposites[turnTo] && this.opposites[turnTo] === this.current) {
+            this.move(turnTo);
+            this.turning = Phaser.NONE;
+            this.turningPoint = new Phaser.Geom.Point();
+        }
+        else {
+            this.turning = turnTo;
+        }
+    }
+
+
+    turn()
+    {
+        if(this.turning === Phaser.NONE) {
+            return false;
+        }
+
+        //  This needs a threshold, because at high speeds you can't turn because the coordinates skip past
+        if (!Phaser.Math.Within(this.sprite.x, this.turningPoint.x, this.threshold) || !Phaser.Math.Within(this.sprite.y, this.turningPoint.y, this.threshold))
+        {
+            return false;
+        }        
         
+        this.sprite.setPosition(this.turningPoint.x, this.turningPoint.y);
+        this.move(this.turning);
+        this.turning = Phaser.NONE;
+        this.turningPoint = new Phaser.Geom.Point();
+        return true;
+    }
 
-        
+    move(direction)
+    {
+        this.playing = true;
+        this.current=direction;
 
-    },
+        switch(direction)
+        {
+            case Phaser.LEFT:
+            this.moveLeft();
+            break;
 
-create: function () 
-{
+            case Phaser.RIGHT:
+            this.moveRight();
+            break;
 
-    
+            case Phaser.UP:
+            this.moveUp();
+            break;
 
-    
-    this.mur.addTilesetImage('mur');
+            case Phaser.DOWN:
+            this.moveDown();
+            break;
+        }
+    }
 
-    this.layer = this.map.createLayer('Desmark-Dev');
+    isSafe(index) {
+        for (let i of this.safetile) {
+            if(i===index) return true;
+        }
 
-    this.dots = this.add.physicsGroup();
+        return false;
+    }
 
-    this.map.createFromTiles(7, this.safetile, 'dot', this.layer, this.dots);
+    drawDebug(graphics) 
+    {
+        let thickness = 4;
+        let alpha = 1;
+        let color = 0x00ff00;
 
-    //  The dots will need to be offset by 6px to put them back in the middle of the grid
-    this.dots.setAll('x', 6, false, false, 1);
-    this.dots.setAll('y', 6, false, false, 1);
+        for (var t = 0; t < 9; t++)
+        {
+            if (this.directions[t] === null || this.directions[t] === undefined)
+            {
+                continue;
+            }
 
-    //  Pacman should collide with everything except the safe tile
-    this.map.setCollisionByExclusion([this.safetile], true, this.layer);
+            if (this.directions[t].index !== -1)
+            {
+                color = 0xff0000;
+            }
+            else
+            {
+                color = 0x00ff00;
+            }
 
-    //  Position Pacman at grid location 14x17 (the +8 accounts for his anchor)
-    this.desmarkDev = this.add.sprite((14 * 16) + 8, (17 * 16) + 8, 'Desmark-Dev', 0);
-    this.desmarkDev.anchor.set(0.5);
-    this.desmarkDev.animations.add('munch', [0, 1, 2, 1], 20, true);
+            graphics.lineStyle(thickness, color, alpha);
+            graphics.strokeRect(this.directions[t].pixelX, this.directions[t].pixelY, 32, 32);
+        }
 
-    this.physics.arcade.enable(this.desmarkDev);
-    this.desmarkDev.body.setSize(16, 16, 0, 0);
+        color = 0x00ff00;
+        graphics.lineStyle(thickness, color, alpha);
+        graphics.strokeRect(this.turningPoint.x, this.turningPoint.y, 1, 1);
 
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.desmarkDev.play('munch');
-    this.move(Phaser.LEFT);
-
-},
+    }
 }
